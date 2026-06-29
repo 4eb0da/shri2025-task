2290f71841261c9e17a0bd0df9f5ae7310bca94d
Aleksandr Nefedov <garret.rus@gmail.com>
2026-06-29 03:30:23 +0300

    Upd

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,19 +1,15 @@
 import { test, expect } from '@playwright/test';
 
 test('should looks same', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
-  await page.waitForLoadState('networkidle')
-  await page.waitForTimeout(1000);
+    await page.goto('http://localhost:3000/');
+    await page.waitForLoadState('networkidle')
+    await page.waitForTimeout(1000);
 
-  await expect(page).toHaveScreenshot();
+    await expect(page).toHaveScreenshot('page.png');
+
+    await page.locator('#tab_kitchen').click();
+    await expect(page).toHaveScreenshot('kitchen.png');
+
+    await page.locator('#tab_all').click();
+    await expect(page).toHaveScreenshot('all.png');
 });
-
-// test('get started link', async ({ page }) => {
-//   await page.goto('https://playwright.dev/');
-
-//   // Click the get started link.
-//   await page.getByRole('link', { name: 'Get started' }).click();
-
-//   // Expects page to have a heading with the name of Installation.
-//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-// });
